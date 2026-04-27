9f5d96196e3e3c964a291543a0aed27594414843
Driver Generator 2 <drivergenerator2@sensirion.com>
2026-04-27 08:27:40 +0000

    Generate SCD30 driver from SCD30 model version 1.1.1

--- sensirion_i2c_scd30/commands.py
+++ sensirion_i2c_scd30/commands.py
@@ -7,7 +7,7 @@
 #
 # Generator:     sensirion-driver-generator 1.6.1
 # Product:       scd30
-# Model-Version: 1.1.0
+# Model-Version: 1.1.1
 #
 """
 The transfer classes specify the data that is transferred between host and sensor. The generated transfer classes
